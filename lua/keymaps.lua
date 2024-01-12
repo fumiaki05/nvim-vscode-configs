bfa7c39bd47bbdaa5c9cd0a325b81e844073d61a
Fumiaki <iso.bass.60@gmail.com>
2024-01-13 01:22:50 +0900

    Clean up plugin lua files

---
+++ lua/keymaps.lua
@@ -0,0 +1,36 @@
+-- Keymaps
+vim.g.mapleader = " "
+
+if vim.g.vscode then
+  vim.keymap.set("n", "<leader>o", "<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>")
+  vim.keymap.set("n", "<leader>d", "<Cmd>call VSCodeNotify('workbench.action.files.save')<CR>")
+  --vim.keymap.set("n", "H", "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>")
+  --vim.keymap.set("n", "L", "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>")
+else
+  vim.keymap.set('i', 'jj', "<Esc>", { silent = true, noremap = true})
+  vim.keymap.set('i', 'っj', "<Esc>", { silent = true, noremap = true})
+  vim.keymap.set('i', '<C-j>', "<Down>", {noremap = true})
+  vim.keymap.set('i', '<C-k>', "<Up>", {noremap = true})
+  vim.keymap.set('i', '<C-h>', "<Left>", {noremap = true})
+  vim.keymap.set('i', '<C-l>', "<Right>", {noremap = true})
+  vim.keymap.set('n', 'あ', "a", {noremap = true})
+  vim.keymap.set('n', 'い', "i", {noremap = true})
+  vim.keymap.set('n', 'う', "u", {noremap = true})
+  vim.keymap.set('n', 'え', "e", {noremap = true})
+  vim.keymap.set('n', 'っy', "yy", {noremap = true})
+  vim.keymap.set('n', 'っd', "dd", {noremap = true})
+  vim.keymap.set('', ';;', "$", {noremap = true})
+  vim.keymap.set('i', '{', '{}<Left>', {noremap = true})
+  vim.keymap.set('i', '[', '[]<Left>', {noremap = true})
+  vim.keymap.set('i', '(', '()<Left>', {noremap = true})
+  vim.keymap.set('i', '\"', '\"\"<Left>', {noremap = true})
+  vim.keymap.set("i", "\'", "\'\'<Left>", {noremap = true})
+  vim.keymap.set('', '<S-h>', '0', {noremap = true})
+  vim.keymap.set('', '<S-l>', '$', {noremap = true})
+  vim.keymap.set('n', '<leader>s', ':w<CR>', {noremap = true})
+  --vim.keymap.set("n", "<leader>o", function() require("telescope.builtin").find_files { hidden = true } end)
+  --vim.keymap.set("n", "<leader>d", "<Cmd>bd<CR>")
+  vim.keymap.set("n", "H", "<Cmd>bp<CR>")
+  vim.keymap.set("n", "L", "<Cmd>bn<CR>")
+
+end
---
+++ lua/options.lua
@@ -0,0 +1,40 @@
+-- Option Configuration
+if vim.fn.has('mac') then
+  vim.cmd('language en_US')
+end
+
+local set = vim.opt
+
+-- Shared Configuration
+set.hls = true
+set.ignorecase = true
+set.smartcase = true
+set.clipboard = "unnamedplus"
+set.whichwrap = "b,s,h,l,<,>,[,],~"
+
+if not vim.g.vscode then
+  set.swapfile = false
+  set.syntax = "on"
+  set.encoding = "utf-8"
+  set.fileencodings = "utf-8"
+  set.updatetime = 300
+  set.title = true
+  -- UI
+  set.number = true
+  set.ruler = true
+  set.cursorline = true
+  -- Tab Control
+  set.shiftwidth = 2
+  set.tabstop = 2
+  set.expandtab = true
+  --
+  set.showmatch = true
+  set.laststatus = 2
+  set.autoindent = true
+  set.smartindent = true
+  set.scrolloff = 6
+  set.list = true
+  set.listchars = { tab = ">-", trail = "-" }
+  set.completeopt = "menuone"
+  set.termguicolors = true
+end

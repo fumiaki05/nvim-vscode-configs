fa2387f0eb7e9a2a4998c2397eeb0faa4d35f41a
Fumiaki <iso.bass.60@gmail.com>
2024-01-14 20:22:05 +0900

    Added: Telescope.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -33,6 +33,10 @@ else
     },
     {'nvim-tree/nvim-tree.lua', dependencies = {'nvim-tree/nvim-web-devicons'}},
     {'akinsho/bufferline.nvim', dependencies = 'nvim-tree/nvim-web-devicons'},
+    {
+      'nvim-telescope/telescope.nvim', tag = '0.1.5',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    }
   }
 
 end
